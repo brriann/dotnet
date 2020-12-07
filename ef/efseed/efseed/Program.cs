c0e5da5736fbd16cf5c9b401b2d147498b899a65
Brian Foster <brriann@gmail.com>
2020-12-06 21:40:34 -0700

    swap EF 6 for EF Core

--- ef/efseed/efseed/Program.cs
+++ ef/efseed/efseed/Program.cs
@@ -3,7 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+// EF 6
+//using System.Data.Entity;
+
+//
+// EF SEEDING
+//
+// https://docs.microsoft.com/en-us/ef/ef6/modeling/code-first/workflows/new-database
+// https://www.entityframeworktutorial.net/efcore/create-model-for-existing-database-in-ef-core.aspx
+//
+// EF 6 vs EF Core
+//
+// https://docs.microsoft.com/en-us/ef/efcore-and-ef6/
+
 
 namespace CodeFirstNewDatabaseSample
 {
@@ -11,6 +24,32 @@ namespace CodeFirstNewDatabaseSample
    {
       static void Main(string[] args)
       {
+         using (var db = new BloggingContext())
+         {
+            // Create and save a new Blog
+            Console.Write("Enter a name for a new Blog: ");
+            var name = Console.ReadLine();
+
+            var blog = new Blog { Name = name };
+            db.Blogs.Add(blog);
+            db.SaveChanges();
+
+            // Display all Blogs from the database
+            var query = from b in db.Blogs
+                        orderby b.Name
+                        select b;
+
+            //var query2 = db.Blogs.OrderBy(b => b.Name);
+
+            Console.WriteLine("All blogs in the database:");
+            foreach (var item in query)
+            {
+               Console.WriteLine(item.Name);
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+         }
       }
    }
 
