476f7e013318fa4ba1521350dac6340afe8ce4b0
Brian Foster <brriann@gmail.com>
2020-12-08 12:36:56 -0700

    AddFieldsToSkier Migration and add in C# additional fields

---
+++ mvc/mvc1/mvc1/Migrations/20201208192750_AddFieldsToSkier.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mvc1.Migrations
+{
+    public partial class AddFieldsToSkier : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Tourney",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Skier",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "BettingLine",
+                table: "Skier",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSlalom",
+                table: "Skier",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsTrick",
+                table: "Skier",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SalaryCap",
+                table: "Skier",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BettingLine",
+                table: "Skier");
+
+            migrationBuilder.DropColumn(
+                name: "IsSlalom",
+                table: "Skier");
+
+            migrationBuilder.DropColumn(
+                name: "IsTrick",
+                table: "Skier");
+
+            migrationBuilder.DropColumn(
+                name: "SalaryCap",
+                table: "Skier");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Tourney",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Skier",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
--- mvc/mvc1/mvc1/Models/Skier.cs
+++ mvc/mvc1/mvc1/Models/Skier.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,16 +13,26 @@ namespace mvc1.Models
       [Key]
       public int Id { get; set; }
       [Required]
+
       public string Name { get; set; }
       [DisplayName("Skier #")]
       [Required]
       [Range(1,int.MaxValue,ErrorMessage ="Skier Number must be greater than 0")]
       public int SkierNumber { get; set; }
+      [Required]
       public bool IsSlalom { get; set; }
+      [Required]
       public bool IsTrick { get; set; }
+      [Column("IsJ")]
+      [Required]
+      public bool IsJump { get; set; }
+      [Required]
       [DisplayName("Salary Cap")]
       public int SalaryCap { get; set; }
       [DisplayName("Betting Line")]
       public double BettingLine { get; set; }
+      [NotMapped]
+      public DateTime LoadedFromDatabase { get; set; }
+      
    }
 }
