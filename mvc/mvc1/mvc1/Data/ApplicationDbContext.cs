9c9326cf0eff3f0875db28bad5b448102cb66804
Brian Foster <brriann@gmail.com>
2020-12-07 17:01:10 -0700

    Entity Framework Migration

---
+++ mvc/mvc1/mvc1/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mvc1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc1.Data
+{
+   public class ApplicationDbContext : DbContext
+   {
+      public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+      {
+
+      }
+
+      public DbSet<Skier> Skier { get; set; }
+   }
+}
---
+++ mvc/mvc1/mvc1/Migrations/20201207234305_addSkierToDatabase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mvc1.Migrations
+{
+    public partial class addSkierToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Skier",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SkierNumber = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Skier", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Skier");
+        }
+    }
+}
