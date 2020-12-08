1eb082b2a4926f6979987902d430a28973820421
Brian Foster <brriann@gmail.com>
2020-12-07 19:36:59 -0700

    skier crud and icons

--- mvc/mvc1/mvc1/Controllers/SkierController.cs
+++ mvc/mvc1/mvc1/Controllers/SkierController.cs
@@ -42,5 +42,66 @@ namespace mvc1.Controllers
          return View(obj);
 
       }
+
+      // GET - EDIT
+      public IActionResult Edit(int? id)
+      {
+         if (id == null || id == 0)
+         {
+            return NotFound();
+         }
+         var obj = _db.Skier.Find(id); // .Find() ==> PK
+         if (obj == null)
+         {
+            return NotFound();
+         }
+         return View(obj);
+      }
+
+      //POST - EDIT
+      [HttpPost]
+      [ValidateAntiForgeryToken]
+      public IActionResult Edit(Skier obj)
+      {
+         if (ModelState.IsValid)
+         {
+            _db.Skier.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+         }
+         return View(obj);
+
+      }
+
+      // GET - DELETE
+      public IActionResult Delete(int? id)
+      {
+         if (id == null || id == 0)
+         {
+            return NotFound();
+         }
+         var obj = _db.Skier.Find(id); // .Find() ==> PK
+         if (obj == null)
+         {
+            return NotFound();
+         }
+         return View(obj);
+      }
+
+      //POST - DELETE
+      [HttpPost]
+      [ValidateAntiForgeryToken]
+      public IActionResult DeletePost(int? id)
+      {
+         var obj = _db.Skier.Find(id);
+         if (obj == null)
+         {
+            return NotFound();
+         }
+         _db.Skier.Remove(obj);
+         _db.SaveChanges();
+         return RedirectToAction("Index");
+
+      }
    }
 }
