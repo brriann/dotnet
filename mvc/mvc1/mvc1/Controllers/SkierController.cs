a61e53a3012794af8f2305b8b432fcb32eb38f3e
Brian Foster <brriann@gmail.com>
2020-12-07 17:33:31 -0700

    create skier form and db save

---
+++ mvc/mvc1/mvc1/Controllers/SkierController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using mvc1.Data;
+using mvc1.Models;
+
+namespace mvc1.Controllers
+{
+    public class SkierController : Controller
+    {
+      private readonly ApplicationDbContext _db;
+
+      public SkierController(ApplicationDbContext db)
+      {
+         _db = db;
+      }
+        public IActionResult Index()
+        {
+         IEnumerable<Skier> skierList = _db.Skier;
+            return View(skierList);
+        }
+
+      // GET - CREATE
+      public IActionResult Create()
+      {
+         return View();
+      }
+
+      //POST - CREATE
+      [HttpPost]
+      [ValidateAntiForgeryToken]
+      public IActionResult Create(Skier obj)
+      {
+         if (ModelState.IsValid)
+         {
+            _db.Skier.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+         }
+         return View(obj);
+
+      }
+   }
+}
--- mvc/mvc1/mvc1/Models/Skier.cs
+++ mvc/mvc1/mvc1/Models/Skier.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ namespace mvc1.Models
       [Key]
       public int Id { get; set; }
       public string Name { get; set; }
+      [DisplayName("Skier #")]
       public int SkierNumber { get; set; }
    }
 }
